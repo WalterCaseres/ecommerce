992b6f11c02c9e1bedfc99227a385bbe8333f596
WalterCaseres <neo.walter66@gmail.com>
2023-06-03 18:01:58 -0300

    Primera Subida

---
+++ src/data.js
@@ -0,0 +1,133 @@
+
+export const data = [
+    {
+        id: 1,
+        nameProduct: "3x2-en-snack-saludable-400gr",
+        price: 2000,
+        img: "../public/recursos-imagenes/3x2-en-snack-saludable-400gr.png",
+        quantity: 1
+    },
+    {
+        id: 2,
+        nameProduct: "box-200-snacks-perros",
+        price: 2000,
+        img: "../public/recursos-imagenes/box-200-snacks-churrasquito-mon-ami-para-perros.png",
+        quantity: 1
+    },
+    {   id: 3, 
+        nameProduct: "box-navideno-mon-ami", 
+        price: 14000, 
+        img: "../public/recursos-imagenes/box-navideno-mon-ami-.png", 
+        quantity: 1 },
+    {
+        id: 4,
+        nameProduct: "caja-3kg-alimento-saludable-3kg-razas-pequenas-medianas",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-3kg-alimento-saludable-3kg-razas-pequenas-medianas.png",
+        quantity: 1
+    },
+    {
+        id: 5,
+        nameProduct: "caja-5kg-alimento-saludable-c-superfood-razas-med-grandes",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-5kg-alimento-saludable-c-superfood-razas-med-grandes.png",
+        quantity: 1
+    },
+    {
+        id: 6,
+        nameProduct: "caja-6-kg-alimento-saludable-para-gatos-csuperfood",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-6-kg-alimento-saludable-para-gatos-csuperfood.png",
+        quantity: 1
+    },
+    {
+        id: 7,
+        nameProduct: "caja-6-kg-alimento-saludalbe-superfood-mediano",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-6-kg-alimento-saludalbe-superfood-razas-pequenas-y-med.png",
+        quantity: 1
+    },
+    {
+        id: 8,
+        nameProduct: "caja-12kg-alimento-saludable-c-superfood-razas-med-grandes",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-12kg-alimento-saludable-c-superfood-razas-med-grandes.png",
+        quantity: 1
+    },
+    {
+        id: 9,
+        nameProduct: "caja-14-kg-alimento-saludable-cachorros-csuperfood",
+        price: 14000,
+        img: "../public/recursos-imagenes/caja-14-kg-alimento-saludable-cachorros-csuperfood.png",
+        quantity: 1
+    },
+    { id: 10, nameProduct: "caja-14kg-alimento-saludable-para-gatos", price: 14000, img: "../public/recursos-imagenes/caja-14kg-alimento-saludable-para-gatos.png", quantity: 1 },
+    { id: 11, nameProduct: "caja-400g-dental-clean-human-grade-para-perros", price: 14000, img: "../public/recursos-imagenes/caja-400g-dental-clean-human-grade-para-perros.png", quantity: 1 },
+    { id: 12, nameProduct: "caja-400g-dental-milk-para-perros", price: 14000, img: "../public/recursos-imagenes/caja-400g-dental-milk-para-perros.png", quantity: 1 },
+    { id: 13, nameProduct: "collagen-chewmedium-500gr", price: 14000, img: "../public/recursos-imagenes/collagen-chew-treat-medium-500gr.png", quantity: 1 },
+    { id: 14, nameProduct: "collagen-chewsmall-400gr", price: 14000, img: "../public/recursos-imagenes/collagen-chew-treat-small-400gr.png", quantity: 1 },
+    { id: 16, nameProduct: "degustacion-dental-mon-ami", price: 14000, img: "../public/recursos-imagenes/degustacion-dental-mon-ami.png", quantity: 1 },
+    { id: 17, nameProduct: "degustacion-mon-ami", price: 14000, img: "../public/recursos-imagenes/degustacion-mon-ami.png", quantity: 1 },
+    { id: 18, nameProduct: "degustacion-snack-mon-ami", price: 14000, img: "../public/recursos-imagenes/degustacion-snack-mon-ami.png", quantity: 1 },
+    {
+        id: 19,
+        nameProduct: "doypack-75g-grille-snack",
+        price: 14000,
+        img: "../public/recursos-imagenes/doypack-75g-churrasquito-grille-snack-saludable-para-perros.png",
+        quantity: 1
+    },
+    { id: 21, nameProduct: "doypack-75g-dental-milk-para-perros", price: 14000, img: "../public/recursos-imagenes/doypack-75g-dental-milk-para-perros.png", quantity: 1 },
+    {
+        id: 22,
+        nameProduct: "doypack-75g-dental-stick-cuidado-dental-para-perros",
+        price: 14000,
+        img: "../public/recursos-imagenes/doypack-75g-dental-stick-cuidado-dental-para-perros.png",
+        quantity: 1
+    },
+    {
+        id: 23,
+        nameProduct: "doypack-75g-pollo-grille-snack-saludable-para-perros",
+        price: 14000,
+        img: "../public/recursos-imagenes/doypack-75g-pollo-grille-snack-saludable-para-perros.png",
+        quantity: 1
+    },
+    { id: 24, nameProduct: "meat-stick-carne-vacuna-200gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-carne-vacuna-200gr.png", quantity: 1 },
+    { id: 25, nameProduct: "meat-stick-carne-vacuna-400gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-carne-vacuna-400gr.png", quantity: 1 },
+    { id: 26, nameProduct: "meat-stick-cordero-200gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-cordero-200gr.png", quantity: 1 },
+    { id: 27, nameProduct: "meat-stick-cordero-400gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-cordero-400gr.png", quantity: 1 },
+    { id: 28, nameProduct: "meat-stick-lamb-100gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-lamb-100gr.png", quantity: 1 },
+    { id: 29, nameProduct: "meat-stick-pollo-200gr", price: 14000, img: "../public/recursos-imagenes/meat-stick-pollo-200gr.png", quantity: 1 },
+    { id: 30, nameProduct: "-meat-stick-pollo-400gr", price: 14000, img: "../public/recursos-imagenes/-meat-stick-pollo-400gr.png", quantity: 1 },
+    {
+        id: 31,
+        nameProduct: "pack-400g-snack-saludable",
+        price: 14000,
+        img: "../public/recursos-imagenes/pack-400g-churrasquito-grille-snack-saludable-para-perros.png",
+        quantity: 1
+    },
+    { id: 32, nameProduct: "pack-400g-dental-stick-talle-l", price: 14000, img: "../public/recursos-imagenes/pack-400g-dental-stick-talle-l-para-perros.png", quantity: 1 },
+    { id: 33, nameProduct: "pack-400g-dental-stick-talle-m", price: 14000, img: "../public/recursos-imagenes/pack-400g-dental-stick-talle-m-para-perros.png", quantity: 1 },
+    { id: 34, nameProduct: "pack-400g-dental-stick-talle-s", price: 14000, img: "../public/recursos-imagenes/pack-400g-dental-stick-talle-s-para-perros.png", quantity: 1 },
+    {
+        id: 35,
+        nameProduct: "pack-400g-snack-pollo-grille",
+        price: 14000,
+        img: "../public/recursos-imagenes/pack-400g-snack-pollo-grille-human-grande-para-perros.png",
+        quantity: 1
+    },
+    { id: 36, nameProduct: "pack-400g-trainee-snack", price: 14000, img: "../public/recursos-imagenes/pack-400g-trainee-snack-saludable-para-perros.png", quantity: 1 },
+    {
+        id: 37,
+        nameProduct: "pack-cachorros-gigantes-84-kg",
+        price: 14000,
+        img: "../public/recursos-imagenes/pack-cachorros-gigantes-84-kg-alimento-saludable-cachorros-csuperfood.png",
+        quantity: 1
+    },
+    { id: 38, nameProduct: "pack-dental-snack-free", price: 14000, img: "../public/recursos-imagenes/pack-dental-snack-free.png", quantity: 1 },
+    { id: 39, nameProduct: "pack-x-6-doypacks-75g-trainee-snack-saludable", price: 14000, img: "../public/recursos-imagenes/pack-x-6-doypacks-75g-trainee-snack-saludable.png", quantity: 1 },
+    { id: 40, nameProduct: "promo-entrenamiento-dental-free-", price: 14000, img: "../public/recursos-imagenes/promo-entrenamiento-dental-free-.png", quantity: 1 },
+    { id: 41, nameProduct: "rutina-dental-progresiva-para-perros-grandes", price: 14000, img: "../public/recursos-imagenes/rutina-dental-progresiva-para-perros-grandes.png", quantity: 1 },
+    { id: 42, nameProduct: "rutina-dental-perros-medianos", price: 14000, img: "../public/recursos-imagenes/rutina-dental-progresiva-para-perros-medianos.png", quantity: 1 },
+    { id: 43, nameProduct: "rutina-dental-perros-pequenos", price: 14000, img: "../public/recursos-imagenes/rutina-dental-progresiva-para-perros-pequenos.png", quantity: 1 },
+    { id: 44, nameProduct: "tratamiento-dental-intensivo", price: 14000, img: "../public/recursos-imagenes/tratamiento-dental-intensivo.png", quantity: 1 }
+];
